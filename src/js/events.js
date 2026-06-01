743e578755755adc24da3363040cd4f555e65721
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-06-01 11:56:03 +0300

    save modal js and events scss

--- src/js/events.js
+++ src/js/events.js
@@ -1,20 +1,27 @@
 import { getEvents } from "./api.js";
+import "./modal.js";
+
 
 const eventsList = document.querySelector(".events__list");
 
 getEvents().then(res => console.log(res));
 
 function createEventsMarkup(array) {
-  return array.map(({ name, images, dates, _embedded }) => {
+  return array.map(({ id, name, images, dates, _embedded, priceRanges }) => {
     return `
-      <li class="events__item">
+      <li class="events__item" 
+        data-id="${id}"
+        data-name="${name}"
+        data-date="${dates.start.localDate}"
+        data-venue="${_embedded.venues[0].name}"
+        data-city="${_embedded.venues[0].city.name}"
+        data-image="${images[0].url}"
+        data-price="${priceRanges ? priceRanges[0].min + '-' + priceRanges[0].max + ' ' + priceRanges[0].currency : 'N/A'}"
+      >
         <article class="event-card">
           <img class="event-card__image" src="${images[0].url}" alt="${name}" />
-
           <h2 class="event-card__title">${name}</h2>
-
           <p class="event-card__date">${dates.start.localDate}</p>
-
           <p class="event-card__place">${_embedded.venues[0].name}</p>
         </article>
       </li>
