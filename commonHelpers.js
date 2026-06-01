aa4fde17bdf591df6eb0a55b3809436fda554d77
NazarBekerskij <bekerskijnazar9@gmail.com>
2026-06-01 08:56:46 +0000

    Deploying to gh-pages from @ NazarBekerskij/event-booster@8dad95b90884542900f5ca9fef7e5fadb8e52ccb 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,27 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const a="https://app.ticketmaster.com/discovery/v2/events.json",l="EVDOv2DA0yM2nfxqUJW4oSne0yWf70dD";async function i(s=0){return await(await fetch(`${a}?apikey=${l}&size=20&countryCode=US&page=${s}`)).json()}const u=document.querySelector(".events__list");i().then(s=>console.log(s));function d(s){return s.map(({name:r,images:o,dates:n,_embedded:e})=>`
-      <li class="events__item">
+import{b as l}from"./assets/vendor-1795e137.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&a(r)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();const u="https://app.ticketmaster.com/discovery/v2/events.json",d="EVDOv2DA0yM2nfxqUJW4oSne0yWf70dD";async function o(n=0){return await(await fetch(`${u}?apikey=${d}&size=20&countryCode=US&page=${n}`)).json()}const p=document.querySelector(".events__list");p.addEventListener("click",n=>{const s=n.target.closest(".events__item");if(!s)return;const{name:c,date:a,venue:e,city:t,image:r,price:i}=s.dataset;l.create(`
+    <div class="modal">
+      <img src="${r}" alt="${c}" />
+      <h2>${c}</h2>
+      <p><b>WHEN:</b> ${a}</p>
+      <p><b>WHERE:</b> ${t}, ${e}</p>
+      <p><b>PRICES:</b> ${i}</p>
+    </div>
+  `).show()});const m=document.querySelector(".events__list");o().then(n=>console.log(n));function f(n){return n.map(({id:s,name:c,images:a,dates:e,_embedded:t,priceRanges:r})=>`
+      <li class="events__item" 
+        data-id="${s}"
+        data-name="${c}"
+        data-date="${e.start.localDate}"
+        data-venue="${t.venues[0].name}"
+        data-city="${t.venues[0].city.name}"
+        data-image="${a[0].url}"
+        data-price="${r?r[0].min+"-"+r[0].max+" "+r[0].currency:"N/A"}"
+      >
         <article class="event-card">
-          <img class="event-card__image" src="${o[0].url}" alt="${r}" />
-
-          <h2 class="event-card__title">${r}</h2>
-
-          <p class="event-card__date">${n.start.localDate}</p>
-
-          <p class="event-card__place">${e.venues[0].name}</p>
+          <img class="event-card__image" src="${a[0].url}" alt="${c}" />
+          <h2 class="event-card__title">${c}</h2>
+          <p class="event-card__date">${e.start.localDate}</p>
+          <p class="event-card__place">${t.venues[0].name}</p>
         </article>
       </li>
-    `).join("")}i().then(s=>{const r=s._embedded.events;u.innerHTML=d(r)});
+    `).join("")}o().then(n=>{const s=n._embedded.events;m.innerHTML=f(s)});
 //# sourceMappingURL=commonHelpers.js.map
