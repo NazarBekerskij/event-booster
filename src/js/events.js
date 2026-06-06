20a3482bcbd3cd452621201022199183b82f8ff0
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-06-06 11:04:40 +0300

    save modal scss and js modal + events

--- src/js/events.js
+++ src/js/events.js
@@ -4,22 +4,16 @@ import "./modal.js";
 const eventsList = document.querySelector(".events__list");
 
 function createEventsMarkup(array) {
-  return array.map(({ id, name, images, dates, _embedded, priceRanges, info, description }) => {
-    
-    const standardPrice = priceRanges ? `${priceRanges[0].min}-${priceRanges[0].max} ${priceRanges[0].currency}` : 'N/A';
-    const vipPrice = priceRanges && priceRanges[1] ? `${priceRanges[1].min}-${priceRanges[1].max} ${priceRanges[1].currency}` : 'N/A';
-    const eventInfo = info || description || "No additional information provided by the organizer.";
+  return array.map(({ id, name, images, dates, _embedded, info, description }) => {
+    const eventInfo = info || description || "No description available.";
 
     return `
       <li class="events__item" 
-        data-id="${id}"
         data-name="${name}"
         data-date="${dates.start.localDate}"
         data-venue="${_embedded.venues[0].name}"
         data-city="${_embedded.venues[0].city.name}"
         data-image="${images[0].url}"
-        data-standard="${standardPrice}"
-        data-vip="${vipPrice}"
         data-info="${eventInfo}"
       >
         <article class="event-card">
