782827cb471207369670c463fa8b926d7e329554
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-06-07 11:48:17 +0300

    save events.js hero.js

--- src/js/events.js
+++ src/js/events.js
@@ -3,6 +3,12 @@ import "./modal.js";
 
 const eventsList = document.querySelector(".events__list");
 
+const searchState = {
+  keyword: "",
+  country: ""
+}
+
+
 function createEventsMarkup(array) {
   return array.map(({ id, name, images, dates, _embedded, info, description }) => {
     const eventInfo = info || description || "No description available.";
@@ -31,3 +37,22 @@ getEvents().then(data => {
   const events = data._embedded.events;
   eventsList.innerHTML = createEventsMarkup(events);
 });
+
+
+
+export function renderEventsPage(page = 0, keyword = searchState.keyword, countryCode = searchState.country) {
+  searchState.keyword = keyword;
+  searchState.country = countryCode;
+
+  getEvents(page, searchState.keyword, searchState.country).then(data => {
+    if (!data._embedded || !data._embedded.events) {
+      eventsList.innerHTML = "<p class='events__not-found'>Nothing found! Try another search.</p>";
+      return;
+    }
+
+    const events = data._embedded.events;
+    eventsList.innerHTML = createEventsMarkup(events);
+  });
+}
+
+renderEventsPage();
